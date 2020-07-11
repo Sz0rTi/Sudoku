702c26e990a12cf43c8e4e66ae7ace79e91faf17
MarcinQ <artilist@tlen.pl>
2020-07-12 01:15:38 +0200

    Add project files.

---
+++ Sudoku/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Sudoku.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sudoku
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Point p = PointToScreen(Mouse.GetPosition(this));
+            Picker picker = new Picker();
+            picker.Left = p.X;
+            picker.Top = p.Y;
+            picker.ShowDialog();
+            if(picker.Value != 0)
+            {
+                (sender as Button).Content = picker.Value.ToString();
+            }
+        }
+    }
+}
---
+++ Sudoku/UserControls/Picker.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sudoku.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Picker.xaml
+    /// </summary>
+    public partial class Picker : Window
+    {
+        public int Value { get; set; }
+        public Picker()
+        {
+            InitializeComponent();   
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Point p = PointToScreen(Mouse.GetPosition(sender as Button));
+            this.Left = p.X;
+            this.Top = p.Y;
+            var btn = (Button)sender;
+            Value = int.Parse((string)btn.Content);
+            this.Close();
+        }
+    }
+}
